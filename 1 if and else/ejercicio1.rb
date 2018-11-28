37bd879b2dc59f6c2d263a0726f8bf9c212240f3
miguel <miguel.contrerastorrejon@gmail.com>
2018-11-28 11:54:54 -0300

    ADD: Ejercicio modificado

--- 1 if and else/ejercicio1.rb
+++ 1 if and else/ejercicio1.rb
@@ -1,6 +1,11 @@
 # Hacer que se cumpla la condición modificando la línea 4.
 
+#a = 2
+#if a == 2
+#  puts 'La condición es verdadera.'
+#end
+
+#terminado
+#condigo reducido
 a = 2
-if a == 2
-  puts 'La condición es verdadera.'
-end
+puts 'la condicion es verdadera' if a == 2
